b9e71e9a886f6a8539769ee6c9aded690d1731d7
ELnar <elnar1995@mail.ru>
2017-09-12 01:58:23 +0300

    переделанная структура

---
+++ ConsoleApplication1/ConsoleApplication1/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public struct Employee
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public int Salary { get; set; }
+
+        public void EmployeeInfo() {
+            Console.WriteLine("Имя {0}\nВозраст{1}\n id {2}", Name, Age, id);
+
+        }
+        
+
+        
+
+
+    }
+}
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,37 +8,31 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        struct Worker
-        {
-            public string name;
-            public string position;
-            public int id;
-            public void InfoWorker()
-            {
-                Console.WriteLine("Имя {0}\n Должность{1}\n Номер Id{2}", name, position, id);
-            }
-        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите кол-во сотрудников\n");
-            int size = Console.Read();
-            Worker[] worker = new Worker[size];
-            for (int i = 0; i < size; i++)
-            {
-                Console.WriteLine("Введите имя сотрудника\n");
-                worker[i].name = Console.ReadLine();
-                Console.WriteLine("Введите должность\n");
-                worker[i].position = Console.ReadLine();
-                Console.WriteLine("Введите Номер ID");
-                worker[i].id = Console.Read();
-                Console.Clear();
+            Employee[] employee;
+            int size=0;
+            Console.WriteLine("Введите кол-во сотрудников");
+            size = Console.Read();
+            employee = new Employee[size];
+            for (int i=0;i<employee.Length;i++) {
+                Console.WriteLine("Введите имя Сотрудника");
+                Console.ReadLine();
+                employee[i].Name = Console.ReadLine();
+                Console.WriteLine("Введите Должность");
+                employee[i].Position = Console.ReadLine();
+                Console.WriteLine("Введите зароботную плату сотрудника");
+                employee[i].Salary = Console.Read();
+                Console.WriteLine("Введите возраст");
+                Console.ReadLine();
+                employee[i].Age = Console.Read();
             }
-            foreach (Worker b in worker)
-            {
-                b.InfoWorker();
+            foreach (var emp in employee) {
+                Console.WriteLine("Имя{0}\nID{1}\n Должность{2}\nЗарплата{3}\nВозраст{4}",emp.Name,emp.id,emp.Position,emp.Salary,emp.Age);
             }
             Console.ReadLine();
-
         }
+            
     }
 }
